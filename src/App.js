6545b1aa49986493226ee76b738c612efe8a0701
Prabir Manna <mannaprabir7@gmail.com>
2023-06-28 08:51:32 +0530

    dropdown app component is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Select from "react-select";
 
-function App() {
+export default function App() {
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "100px auto",
+    width: "50%",
+  };
+  const [value, setValue] = React.useState(null);
+
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle}>
+      <label> What is your choice </label>
+      <Select
+        options={options}
+        defaultValue={value}
+        placeholder="Select your optiion"
+        onChange={setValue}
+        noOptionsMessage={() => "No item found..."}
+      />
     </div>
   );
 }
-
-export default App;
